2d3ed60d8fd1259bead1e93be1ca548961e7b984
Rahul <7678478061k@gmail.com>
2024-05-04 11:57:52 +0530

    added basic homepage ui

---
+++ client/src/components/home/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="border border-black px-4 py-2">
+      <div>
+        <h1 className="text-2xl font-bold">FileFlow</h1>
+      </div>
+    </header>
+  );
+};
+export default Header;
